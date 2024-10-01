cc9ba363ca6e6b5c44655df3c6d46e1b01aa380d
Justin Winkler <152222211+ThatGuyWink@users.noreply.github.com>
2024-09-30 22:33:43 -0400

    Update ListItem.java

--- src/main/java/com/cen3024c/app/ListItem.java
+++ src/main/java/com/cen3024c/app/ListItem.java
@@ -1,2 +1,40 @@
-package com.cen3024c.app;public class ListItem {
+package com.cen3024c.app;
+
+/**
+ * The ListItem class represents a single to-do list item.
+ */
+
+public class ListItem {
+    private String description;
+
+    /**
+     * Constructs a new ListItem with the specified description.
+     *
+     * @param description The description of the to-do list item.
+     */
+
+    public ListItem(String description) {
+        this.description = description;
+    }
+
+    /**
+     * Returns the description of the to-do list item.
+     *
+     * @return The description of the item.
+     */
+
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Returns the string representation of the list item.
+     *
+     * @return The description of the item as a string.
+     */
+
+    @Override
+    public String toString() {
+        return description;
+    }
 }
